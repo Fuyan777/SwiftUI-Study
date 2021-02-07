d61ec5839de8340942e8e80d0d2edcad743fca65
Fuyan777 <fuyamiwa713@gmail.com>
2021-02-07 20:25:45 +0900

    [add]widget追加

---
+++ widget/widget/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  widget
+//
+//  Created by 山田楓也 on 2021/02/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
